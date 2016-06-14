c22974166ea2b1f02438ea20478f8b92c5e6e85a
Michael Boreham <bor3ham@gmail.com>
2016-06-14 17:19:32 +1000

    output as var

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,5 +1,7 @@
 var config = require('./webpack.config');
 
 config.externals = {};
+config.output.library = 'interactiveTutorials';
+config.output.libraryTarget = 'var';
 
 module.exports = config;
